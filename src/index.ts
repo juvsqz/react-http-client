753fe0a0c71292b7d459be785dc6d66afa8ea584
Julius Vasquez <julius.vasquez@fissara.com>
2021-03-16 03:03:22 +0800

    chore: Added the typscript compiler build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import HttpClientContextConfig from './context-config';
 import useHttpClient from './use-http-client';
-
-export * from './types';
 export { useHttpClient, HttpClientConfig };
 const HttpClientConfig = HttpClientContextConfig.Provider;
