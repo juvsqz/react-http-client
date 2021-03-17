0188b69edbb75ed3ca41ec472138d533925bbe05
Julius Vasquez <julius.vasquez@fissara.com>
2021-03-18 01:15:40 +0800

    feat: Expose additional typings for hook
    Includes:
    - HttpClientResponse,
    - HttpClientConfiguration,
    - HttpErrorResponse

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import HttpClientContextConfig from './context-config';
 import useHttpClient from './use-http-client';
 const HttpClientConfig = HttpClientContextConfig.Provider;
-export { HttpClientConfig };
+export { HttpClientConfig, useHttpClient };
+export {
+  HttpClientResponse,
+  HttpClientConfiguration,
+  HttpErrorResponse
+} from './types';
 export default useHttpClient;
